c0aed82fc39b9b900b604e0d21ebba176609e1aa
Viktorr <prokat022@gmail.com>
2026-06-05 16:52:54 +0700

    feat: add a local API Gateway chart

--- k8s-terraform-layer/modules/helm-charts-infra/gateway-api.tf
+++ k8s-terraform-layer/modules/helm-charts-infra/gateway-api.tf
@@ -1,10 +1,10 @@
 # k8s-terraform-layer/modules/helm-charts-infra/gateway-api.tf
 
 resource "helm_release" "gateway_api" {
-  name             = "eg"
-  repository       = "oci://docker.io/envoyproxy"
-  chart            = "gateway-helm"
-  version          = "v1.6.0"
+  name             = "gateway-helm"
+  # repository       = "oci://docker.io/envoyproxy" 
+  chart            = "${path.module}/charts/gateway-helm"
+  # version          = "v1.8.1"
   namespace        = var.gateway_api_namespace
   create_namespace = false
   timeout          = 600
