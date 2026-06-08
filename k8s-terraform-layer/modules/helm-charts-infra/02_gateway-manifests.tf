2eed233a992d30035da089f7266f9ad74e61ec73
Viktorr <prokat022@gmail.com>
2026-06-08 15:21:03 +0700

    feat: add gateway manifests and rename files

---
+++ k8s-terraform-layer/modules/helm-charts-infra/02_gateway-manifests.tf
@@ -0,0 +1,27 @@
+resource "kubectl_manifest" "gateway_manifests" {
+  yaml_body = <<-YAML
+    apiVersion: gateway.networking.k8s.io/v1
+    kind: GatewayClass
+    metadata:
+      name: eg
+    spec:
+      controllerName: gateway.envoyproxy.io/gatewayclass-controller
+    ---
+    apiVersion: gateway.networking.k8s.io/v1
+    kind: Gateway
+    metadata:
+      name: eg
+      namespace: ${var.gateway_api_namespace}
+    spec:
+      gatewayClassName: eg
+      listeners:
+        - name: http
+          protocol: HTTP
+          port: 80
+  YAML
+
+  depends_on = [
+    kubectl_manifest.gateway_api_crds,
+    helm_release.gateway_api
+  ]
+}
